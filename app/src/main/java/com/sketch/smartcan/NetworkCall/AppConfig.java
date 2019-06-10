d56219ec8fbca8b08a330aea4d83593a37a98aee
Shadab <shadabm10@gmail.com>
2019-06-10 11:45:26 +0530

    BASE URL

--- app/src/main/java/com/sketch/smartcan/NetworkCall/AppConfig.java
+++ app/src/main/java/com/sketch/smartcan/NetworkCall/AppConfig.java
@@ -2,10 +2,11 @@ package com.sketch.smartcan.NetworkCall;
 
 public class AppConfig
 {
-    public static String URL_DEV = "http://lab-3.sketchdemos.com/P-1166-dominator/cadmin.php/webservice/";
-    public static String IMAGE_PATH="http://lab-3.sketchdemos.com/P-1166-dominator/upload/product/";
-    //public static String URL_DEV = "https://findusonweb.com/rest/RestController.php";
-    //   public static String URL_LOGIN = "https://findusonweb.com/rest/RestController.php";
+    private static String BASE_URL = "http://lab-3.sketchdemos.com/P1193_SmartCan/Api/";
+
+
+    public static String  REGISTRATION=BASE_URL+"registration";
+    public static String  LOGIN=BASE_URL+"login";
 
 
 }
